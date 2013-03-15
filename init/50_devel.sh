0eb603b6646be54398bae6bbacc01506a43d2966
Carl Scharenberg <carl.scharenberg@gmail.com>
2013-03-15 09:19:41 -0500

    make pip use /home/carl.scharenberg, and set it on install.

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -1,2 +1,14 @@
+# At this point, pip.conf has been copied into the
+# home folder (~/.pip/pip.conf) but it needs to be
+# modified with the actual home folder of our user, 
+# so do a replacement on the word HOMEDIR and replace
+# with value of $HOME, using 'ex' editor.
+echo "%s/HOMEDIR/$HOME
+w
+q
+" | ex $HOME/.pip/pip.conf
+
+
+
 source ~/.dotfiles/source/50_devel.sh
 
