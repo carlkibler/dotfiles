ae7fc23b93fd18912c6f16c15fdeadf23d2bfe44
Carl Scharenberg <carl.scharenberg@gmail.com>
2013-03-15 11:32:01 -0500

    add aliases for running on EC2 instance

--- source/10_misc.sh
+++ source/10_misc.sh
@@ -16,3 +16,7 @@ alias nowtime=now
 alias nowdate='date +"%d-%m-%Y"'
 alias path='echo -e ${PATH//:/\\n}'
 
+# for Amazon EC2 instances
+alias public_ip="curl http://169.254.169.254/latest/meta-data/public-ipv4"
+alias private_ip="curl http://169.254.169.254/latest/meta-data/local-ipv4"
+
